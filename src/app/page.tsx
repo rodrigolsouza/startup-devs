0e8f440a5aa0e483d53582eb1a6c4c612f8a1f25
Rinaldo <rinaldoalvesvilela@hotmail.com>
2023-11-15 00:02:23 -0300

    🐛 Fix bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,11 @@
-'use client';
+"use client";
 import { ContactUs } from "./components/Contact/section-contact-us";
 import { Header } from "./components/Header/Header";
-import { Navbar } from "./components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main>
-      <Header/>
+      <Header />
       <ContactUs src="images/contact-img.avif"></ContactUs>
     </main>
   );
