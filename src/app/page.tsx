97084e34fae5080e1a439c1dafe7257fb7fca9b6
Rinaldo <rinaldoalvesvilela@hotmail.com>
2023-11-15 02:35:44 -0300

    🐛  Corrigindo conflitos

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,11 @@ import { SectionHome } from "./components/Home/SectionHome";
 
 
 
+
 export default function Home() {
   return (
     <main>
-      <Header/>
+      <Header></Header>
       <SectionHome imgSrc="https://cdn.lugc.link/ceb0edac-1016-4630-a72a-4b5d92857a6d/-/preview/-/stretch/off/-/format/auto/-/quality/normal/-/resize/x603/"
         title="digital marketing agency."
         subtitle=""
