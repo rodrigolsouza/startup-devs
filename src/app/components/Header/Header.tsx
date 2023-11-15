40abef691861ab58844ce32504c2d2c723216cce
Rinaldo <rinaldoalvesvilela@hotmail.com>
2023-11-14 23:16:18 -0300

    ✨ Add: Header

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,39 @@
+import "./styleHeader.css";
+import React from "react";
+import { useState } from "react";
+
+export function Header() {
+  const [activeLink, setActiveLink] = useState(null);
+
+  const handleLinkClick = (index: any) => {
+    setActiveLink((prevActiveLink) =>
+      prevActiveLink === index ? null : index
+    );
+  };
+
+  return (
+    <div className="page-header__row">
+      <div className="logo">Logo</div>
+      <nav className="main-navigation">
+        <ul>
+          {["home", "services", "portfolio", "about", "contact"].map(
+            (section, index) => (
+              <li key={index}>
+                <a
+                  href={`#${section}`}
+                  className={`nav-link ${activeLink === index ? "active" : ""}`}
+                  onClick={() => handleLinkClick(index)}
+                >
+                  {section}
+                </a>
+              </li>
+            )
+          )}
+        </ul>
+      </nav>
+      <div className="cta-button">
+        <button>Contato</button>
+      </div>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
-
-
-import { ContactUs } from './components/Contact/section-contact-us'
+'use client';
+import { ContactUs } from "./components/Contact/section-contact-us";
+import { Header } from "./components/Header/Header";
+import { Navbar } from "./components/Navbar/Navbar";
 
 export default function Home() {
   return (
     <main>
+      <Header/>
       <ContactUs src="images/contact-img.avif"></ContactUs>
     </main>
-  )
+  );
 }
