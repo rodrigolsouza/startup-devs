e83a33fa6ac3da3788c037a4530668ab81192508
Rinaldo <rinaldoalvesvilela@hotmail.com>
2023-11-15 03:41:33 -0300

    💄 ✅ update style

--- src/app/components/Home/SectionHome.tsx
+++ src/app/components/Home/SectionHome.tsx
@@ -31,17 +31,20 @@ export function SectionHome({
         </div>
       </div>
 
-      <div className="shape-container">
-          <img className="cube-one" src="https://cdn.lugc.link/3460bcf5-cd68-454e-9e4d-b7a338ac55f6/-/stretch/off/-/format/auto/-/quality/normal/-/resize/56x/" alt="Circle" />
-          <img className="cube-two" src="https://cdn.lugc.link/6e88c0b1-fdd0-4df2-a546-630f09d735a0/-/stretch/off/-/format/auto/-/quality/normal/-/resize/53x/" alt="Square" />
-          <img className="pyramid" src="https://cdn.lugc.link/81472342-e225-4655-935c-5d1afd2eb887/-/stretch/off/-/format/auto/-/quality/normal/-/resize/87x/" alt="Triangle" />
-        </div>
+      
       
 
       <div className="image-container-home">
-        <img src={imgSrc} alt="Right Image" />
+        <img className="image-people" src={imgSrc} alt="Right Image" />
+        <div className="shape">
+        <img className="cube-one" src="https://cdn.lugc.link/3460bcf5-cd68-454e-9e4d-b7a338ac55f6/-/stretch/off/-/format/auto/-/quality/normal/-/resize/56x/" alt="Circle" />
+        <img className="cube-two" src="https://cdn.lugc.link/6e88c0b1-fdd0-4df2-a546-630f09d735a0/-/stretch/off/-/format/auto/-/quality/normal/-/resize/53x/" alt="Square" />
+        <img className="pyramid" src="https://cdn.lugc.link/81472342-e225-4655-935c-5d1afd2eb887/-/stretch/off/-/format/auto/-/quality/normal/-/resize/87x/" alt="Triangle" />
+        </div>
+        
 
       </div>
+
     </section>
   );
 }
