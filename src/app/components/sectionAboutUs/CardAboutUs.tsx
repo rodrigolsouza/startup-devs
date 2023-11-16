d3f609ca3ba64bce4b22fbca457add9b22c52feb
Rodrigo Souza <rodrigo.lopessouza@hotmail.com>
2023-11-15 21:10:04 -0300

    ✅ Feature finished

--- src/app/components/sectionAboutUs/CardAboutUs.tsx
+++ src/app/components/sectionAboutUs/CardAboutUs.tsx
@@ -1,30 +1,50 @@
-import React from 'react';
-import {Avatar} from './Avatar';
-import "./cardAboutUs.css"
-
+"use client";
+import React from "react";
+import { Avatar } from "./Avatar";
+import "./cardAboutUs.css";
+import { useState } from "react";
 
 export type ListPeopleProps = {
-    people: {
-      id?: string;
-      image: string;
-      name: string;
-      jobTitle: string;
-    }[];
-  };
+  people: {
+    id?: string;
+    image: string;
+    name: string;
+    jobTitle: string;
+  }[];
+};
 
-export function CardAboutUs({people}:ListPeopleProps) {
+export function CardAboutUs({ people }: ListPeopleProps) {
+  const [activeButton, setActiveButton] = useState(false);
+
+  const handleButtonClick = () => {
+    setActiveButton(!activeButton); 
+  };
   return (
-    <div className='card-container'>
-      <h1>About Us</h1>
-      <p>A bit about us</p>
-      <div className='list-avatar'>
-        {people.map((person)=>(
-            <Avatar key={person.id} people={person}/>
+    <div className="card-container">
+      <h1 className="title-about-us">ABOUT US</h1>
+      <div className="list-avatar">
+        {people.map((person) => (
+          <Avatar key={person.id} people={person} />
         ))}
       </div>
-      
+
+      <button className="button-about-us" onClick={handleButtonClick}>
+        A bit about us
+      </button>
+      <p
+        className={
+          activeButton ? "text-bit-about-us-active" : "text-bit-about-us"
+        }
+      >
+        We are a web and mobile application development startup. We create
+        intelligent digital platforms for you, the entrepreneur, to modernize
+        your business and boost your sales by offering innovative experiences
+        for your customers. Our team of developers are trained in the most
+        current tools on the technology market such as React and Next.ts, in
+        addition to the best known tools such as HTML5, SASS and JavaScript.
+      </p>
     </div>
   );
-};
+}
 
 export default CardAboutUs;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-
+"use client"
 
 import {CardAboutUs} from "./components/sectionAboutUs/CardAboutUs";
 
@@ -8,27 +8,27 @@ export default function Home() {
   const listPeople=[
     {
         id: crypto.randomUUID(),
-        image:"https://www.vidadeturista.com/wp-content/uploads/2009/03/recife-pe-530x398.jpg",
+        image:"https://avatars.githubusercontent.com/u/61033232?v=4",
         name:"Rodrigo Souza",
         jobTitle: "Developer",
     },
     {   
         id: crypto.randomUUID(),
-        image:"https://www.vidadeturista.com/wp-content/uploads/2009/03/recife-pe-530x398.jpg",
+        image:"https://images-ext-1.discordapp.net/external/78utGeiONFJmT5_6UTlS2Cfw8BJO5Idm6rFo_r6LsRw/%3Fe%3D1705536000%26v%3Dbeta%26t%3Dl8j7JsbbAtAtkpjo1hBDqwNCyUbIOdf03AK0Wg6rtJM/https/media.licdn.com/dms/image/D4D03AQGRXabDApjhQQ/profile-displayphoto-shrink_800_800/0/1697927837701?width=418&height=418",
         name:"Emesson",
-        jobTitle: "Developer",
+        jobTitle: "Programming",
     },
     {
         id: crypto.randomUUID(),
-        image:"https://www.vidadeturista.com/wp-content/uploads/2009/03/recife-pe-530x398.jpg",
+        image:"https://avatars.githubusercontent.com/u/134212408?v=4",
         name:"Rinaldo Vilela",
         jobTitle: "Developer",
     },
     {
         id: crypto.randomUUID(),
-        image:"https://www.vidadeturista.com/wp-content/uploads/2009/03/recife-pe-530x398.jpg",
+        image:"https://avatars.githubusercontent.com/u/114183514?v=4",
         name:"Nemésio",
-        jobTitle: "Developer",
+        jobTitle: "Programming",
     }
   ]
 
