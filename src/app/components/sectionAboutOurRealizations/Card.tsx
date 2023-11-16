d9ab81bfe7976f7dd17c5b68a26e354c0f767eee
NemesioCarlos <nemesio.carlos97@gmail.com>
2023-11-16 12:16:10 -0300

    create-section-our-realization

---
+++ src/app/components/sectionAboutOurRealizations/Card.tsx
@@ -0,0 +1,12 @@
+import "./style.css";
+
+function Card() {
+  return (
+    <div className="card">
+        <h2 className="card-title">Qualidade:</h2>
+        <span className="span"> Nós nos esforçamos para fornecer resultados de alta qualidade em todos os nossos projetos.</span>
+    </div>
+    )
+}
+
+export default Card;
